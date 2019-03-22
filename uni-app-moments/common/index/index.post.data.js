3a474495562c7cf67b7a4033b9528c5112cf58cf
liuxy <2199842341@qq.com>
2019-03-22 09:10:17 +0800

    修复H5下图片预览时总显示第一张的问题
    本地测试时将展示数据中图片路径改为绝对路径,项目上线后数据均为后端获取,图片地址为网络地址,不存在此问题

--- uni-app-moments/common/index/index.post.data.js
+++ uni-app-moments/common/index/index.post.data.js
@@ -2,10 +2,10 @@ export default [{
 							"post_id": '1',
 							"uid": 1,
 							"username": "龙葵",
-							"header_image": "../../static/index/test/header03.jpg",
+							"header_image": "/static/index/test/header03.jpg",
 							"content": {
 								"text": "内裤上百条，晒不干一条；衣服晾不干，亲人泪两行",
-								"images": ["../../static/index/test/test2.jpg"]
+								"images": ["/static/index/test/test2.jpg"]
 							},
 							"islike": 0,
 							"like": [{
@@ -37,19 +37,19 @@ export default [{
 							"post_id": 2,
 							"uid": 1,
 							"username": "菁英公寓-打造属于你的私密空间 小吴",
-							"header_image": "../../static/index/test/header04.jpg",
+							"header_image": "/static/index/test/header04.jpg",
 							"content": {
 								"text": "租房:东环朝南\n\r2室大衣柜\n\r燃气热水器\n\r5楼采光充足\n\r随时入住",
 								"images": [
-									"../../static/index/test/pig-01.jpg", 
-									"../../static/index/test/pig-02.jpg",
-									"../../static/index/test/pig-03.jpg", 
-									"../../static/index/test/pig-04.jpg",
-									"../../static/index/test/pig-05.jpg",
-									"../../static/index/test/pig-06.jpg",
-									"../../static/index/test/pig-07.jpg",
-									"../../static/index/test/pig-08.jpg",
-									"../../static/index/test/pig-09.jpg"
+									"/static/index/test/pig-01.jpg", 
+									"/static/index/test/pig-02.jpg",
+									"/static/index/test/pig-03.jpg", 
+									"/static/index/test/pig-04.jpg",
+									"/static/index/test/pig-05.jpg",
+									"/static/index/test/pig-06.jpg",
+									"/static/index/test/pig-07.jpg",
+									"/static/index/test/pig-08.jpg",
+									"/static/index/test/pig-09.jpg"
 								]
 							},
 							"islike": 0,
@@ -82,11 +82,11 @@ export default [{
 							"post_id": 2,
 							"uid": 1,
 							"username": "BSK 必胜客新苏  小乐",
-							"header_image": "../../static/index/test/header05.jpg",
+							"header_image": "/static/index/test/header05.jpg",
 							"content": {
 								"text": "美食花样多，诱人如北北；迎来小宇宙，幸福两行泪[喵喵]这可是小必的心声啊～",
-								"images": ["../../static/index/test/header01.jpg", "../../static/index/test/header01.jpg",
-									"../../static/index/test/header01.jpg", "../../static/index/test/header01.jpg"
+								"images": ["/static/index/test/header01.jpg", "/static/index/test/header01.jpg",
+									"/static/index/test/header01.jpg", "/static/index/test/header01.jpg"
 								]
 							},
 							"islike": 0,
